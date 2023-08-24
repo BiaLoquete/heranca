6adfa67e706abb8670a943e063e1bc009691ae1b
BiaLoquete <124930485+BiaLoquete@users.noreply.github.com>
2023-08-24 10:57:18 -0300

    Add files via upload

---
+++ herança/ex2.py
@@ -0,0 +1,16 @@
+class Animal:
+    def __init__(self, nome):
+        self.nome = nome
+    def fazer_som(self):
+        print('som')
+
+class Cachorro(Animal):
+    def __init__(self,nome):
+        super().__init__(nome)
+
+    def fazer_som(self):
+        print("Auau")
+
+Liz = Cachorro("Liz")
+Liz.fazer_som()
+
